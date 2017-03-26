b35ea06bd197f7cbe858db91ea2dd7cd9f4a29d4
l1000965431 <l1000965431@126.com>
2017-03-26 11:34:41 +0800

    添加文件
    添加文件

---
+++ LuaTool.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using LuaFramework;
+
+public class LuaTool : EditorWindow
+{
+    //lua文件路径
+    string luaFilePath ="";
+
+    [MenuItem("LuaTool/DoFile", false, 102)]
+    static void DoIt()
+    {
+        LuaTool lua = GetWindow<LuaTool>();
+        lua.Show();
+    }
+
+    private void OnGUI()
+    {
+        luaFilePath = EditorGUILayout.TextField("输入要执行的lua文件路径:", luaFilePath);
+        if (GUILayout.Button("DoFile"))
+        {
+            DoLuaFile();
+        }
+    }
+
+
+    private void DoLuaFile()
+    {
+        if(luaFilePath.Length == 0)
+        {
+            EditorGUILayout.HelpBox("文件路径错误", MessageType.None);
+            return;
+        }
+
+        if (!File.Exists(luaFilePath))
+        {
+          EditorGUILayout.HelpBox("文件不存在", MessageType.None);
+          return;
+        }
+
+        LuaManager mgr = AppFacade.Instance.GetManager<LuaManager>(ManagerName.Lua);
+        mgr.DoFile(luaFilePath);
+    }
+}
